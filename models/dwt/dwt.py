55f22cec5d56721085d24017e919e98cd3d827d7
alexanderkuebel <alexander.kuebel@code.berlin>
2021-10-13 18:48:39 +0200

    add dwt to dc-models

---
+++ models/dwt/dwt.py
@@ -0,0 +1,159 @@
+import pywt
+import numpy as np
+
+# from benchmarking.compression.benchmarks.compression_ratio import get_compression_ratio
+# from benchmarking.compression.benchmarks.reconstruction_error import get_reconstruction_error
+from typing import Any, List
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+
+
+wavelet = "db1"
+
+batch_size: int = 1
+sequence_length: int = 20
+
+
+def read_and_preprocess_data(
+    should_smooth: bool = False,
+    smoothing_window: int = 100,
+    sequence_length: int = 120,
+    cut_off_min: int = 5,
+    cut_off_max: int = 45,
+    should_scale: bool = True,
+    data_path: str = "data/datasets/data.txt",
+    batch_size: int = 32,
+    motes_train: List = [1, 2, 3, 4, 6, 7, 9, 10, 32, 34, 35],
+    motes_test: List = [36],
+) -> Any:
+    """
+    Load the temperature sensor data of the "Intel Berkeley Research Lab" dataset, clean it and scale it down.
+
+    :parameters:
+    cut_off_min(number)   -- threshhold to discard all temperatures below that point
+    cut_off_max(number)   -- threshhold to discard all temperatures above that point
+    should_scale(boolean) -- switch between min-max-scaling data or not
+    data_path(string)     -- path to the file containing all data
+
+    :returns:
+    x_train -- numpy array of shape dictated by config and train_range
+    x_test  -- numpy array of shape dictated by config and test_range
+    config  -- chosen config in case it needs to be reused later on
+    """
+
+    # Load, clean and preprocess data
+    df = pd.read_csv(
+        data_path,
+        sep=" ",
+        lineterminator="\n",
+        names=[
+            "date",
+            "time",
+            "epoch",
+            "moteid",
+            "temperature",
+            "humidity",
+            "light",
+            "voltage",
+        ],
+    )
+
+    # Clean nans
+    df.dropna(inplace=True)
+
+    # Clean outliers
+    df.drop(
+        df[(df["temperature"] < cut_off_min) | (df["temperature"] > cut_off_max)].index,
+        inplace=True,
+    )
+
+    # temperature_std = df["temperature"].std()
+    lower_bound = df["temperature"].mean() - 3 * df["temperature"].std()
+    upper_bound = df["temperature"].mean() + 3 * df["temperature"].std()
+    df.drop(
+        df[(df["temperature"] < lower_bound) | (df["temperature"] > upper_bound)].index,
+        inplace=True,
+    )
+
+    def concat_motes(mote_ids: List) -> pd.DataFrame:
+        # Concatenate all relevant motes into one dataframe
+        tmp_frames = []
+        for mote_id in mote_ids:
+            tmp_frame = df.loc[df["moteid"] == mote_id]["temperature"]
+            tmp_frame = tmp_frame.reset_index(drop=True)
+            tmp_frames.append(tmp_frame)
+        return pd.concat(tmp_frames, axis=0)
+
+    x_train = concat_motes(motes_train)
+    x_test = concat_motes(motes_test)
+
+    if should_smooth:
+        x_train = x_train.rolling(window=smoothing_window).mean()[smoothing_window:]
+        x_test = x_test.rolling(window=smoothing_window).mean()[smoothing_window:]
+
+    if should_scale:
+        scaler = MinMaxScaler()
+        x_train = scaler.fit_transform(x_train.values.reshape(-1, 1))
+        x_test = scaler.fit_transform(x_test.values.reshape(-1, 1))
+
+    ###
+    # Prepare the data
+    ###
+    def reshape_inputs(data: Any) -> Any:
+        assert sequence_length <= data.shape[0]
+        remainder = data.shape[0] % sequence_length
+        limit = data.shape[0] - remainder
+        data = data[:limit, :]
+        n_samples = int(data.shape[0] / sequence_length)
+
+        n_dims = data.shape[1]
+        reshaped_data = data.reshape(n_samples, sequence_length, n_dims)
+
+        length = reshaped_data.shape[0]
+        cutoff = length % batch_size
+        new_length = length - cutoff
+        return reshaped_data[:new_length]
+
+    x_train = reshape_inputs(x_train)
+    x_test = reshape_inputs(x_test)
+
+    return x_train, x_test
+
+
+x_train, x_test = read_and_preprocess_data(
+    sequence_length=sequence_length,
+    batch_size=batch_size,
+    motes_train=[7],
+    motes_test=[7],
+)
+
+x_train = x_train[:1900, :, :]
+x_test = x_test[1900:, :, :]
+
+
+def get_prms_diff(original: Any, prediction: Any, to_numpy: bool = True) -> Any:
+    diff = tf.reduce_sum(tf.square(tf.subtract(original, prediction)))
+    sq = tf.reduce_sum(tf.square(original))
+    prms = 100 * (tf.sqrt(tf.divide(diff, sq + 0.0e-6)))
+
+    return prms.numpy() if to_numpy else prms
+
+
+def dwt(time_series):
+    for time_step in time_series:
+        coeffs = pywt.wavedec(time_step, wavelet=wavelet)
+
+        coeff_arr, coeff_slices = pywt.coeffs_to_array(coeffs)
+
+        Csort = np.sort(np.abs(coeff_arr.reshape(-1)))
+
+        # for keep in (0.1, 0.05, 0.01, 0.005):
+        keep = 0.1
+        number = int(np.floor((1 - keep) * len(Csort)))
+        thresh = Csort[number]
+        ind = np.abs(coeff_arr) > thresh
+        Cfilt = coeff_arr * ind
+
+        coeffs_filt = pywt.array_to_coeffs(Cfilt, coeff_slices, output_format="wavedec")
+        Arecon = pywt.waverec(coeffs_filt, wavelet=wavelet)
+        print(Arecon, time_step)
